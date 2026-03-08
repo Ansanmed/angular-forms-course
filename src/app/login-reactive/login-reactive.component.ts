2986f630d9d473dbda292673c4d5bffba6ca0657
Antonio <antoniosanmed@gmail.com>
2026-03-08 14:06:12 +0100

    feature: rework login form

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { createPasswordStrengthValidator } from '../validators/password-validator';
 
 @Component({
   selector: 'login',
@@ -8,19 +10,45 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   standalone: false,
 })
 export class LoginReactiveComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+
+  /* Esta es la forma antigua de hacerlo, instanciando directamente el form group
   email = new FormControl('', {
     validators: [Validators.required, Validators.email],
     updateOn: 'blur',
   });
 
   password = new FormControl('', {
-    validators: [Validators.required, Validators.minLength(8)],
+    validators: [Validators.required, Validators.minLength(8), createPasswordStrengthValidator()],
   });
 
-  form = new FormGroup({
+  formInstanciationgFormGroup = new FormGroup({
     email: this.email,
     password: this.password,
   });
+
+  */
+  form = this.fb.group({
+    email: [
+      '',
+      {
+        validators: [Validators.required, Validators.email],
+        updateOn: 'blur',
+      },
+    ],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(8), createPasswordStrengthValidator()],
+    ],
+  });
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  get password() {
+    return this.form.controls.password;
+  }
 
   constructor() {}
 
